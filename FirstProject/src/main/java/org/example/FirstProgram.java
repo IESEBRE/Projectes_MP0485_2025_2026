aa5fe00f5f99faebed1345a37602cc5bee768a29
Santi Sabaté <ssabate@iesebre.com>
2025-09-19 10:00:11 +0200

    Explicació de tipos de dades

--- FirstProject/src/main/java/org/example/FirstProgram.java
+++ FirstProject/src/main/java/org/example/FirstProgram.java
@@ -1,4 +1,25 @@
 package org.example;
 
 public class FirstProgram {
+
+    public static void main(String[] args) {
+
+        //Declaració de variables
+        int edatPersona=20;  //Noms incorrectes: EdatPersona, edatpersona, EDATPERSONA
+
+        edatPersona=56;
+
+        System.out.println("Hello world!!");
+        System.out.println("Segona línia!!"+"20");
+        System.out.println(edatPersona);
+
+        edatPersona= edatPersona + 10;  // + sumar
+        edatPersona= edatPersona - 10;  // - restar
+        edatPersona= edatPersona * 10;  // * multiplicar
+        edatPersona= edatPersona / 10;  // / divisió entera
+        edatPersona= edatPersona % 10;  // % resto, mòdul de la divisió entera
+
+
+        System.out.println("Esta persona té una edat de " + edatPersona);
+    }
 }
--- FirstProject/src/main/java/org/example/Main.java
+++ FirstProject/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+/*    static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         IO.println(String.format("Hello and welcome!"));
@@ -13,5 +13,5 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             IO.println("i = " + i);
         }
-    }
+    }*/
 }
