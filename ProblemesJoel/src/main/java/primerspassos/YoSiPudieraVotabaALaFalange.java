100053ca41b0bb16c36fe3df57e92313029dd9a7
Santi Sabaté <ssabate@iesebre.com>
2025-10-03 09:59:14 +0200

    Afegixo idea per fer el de la Falange

---
+++ ProblemesJoel/src/main/java/primerspassos/YoSiPudieraVotabaALaFalange.java
@@ -0,0 +1,8 @@
+package primerspassos;
+
+public class YoSiPudieraVotabaALaFalange {
+
+    public static void main(String[] args) {
+        //Sumar la diferencia entre els 3 partits i mirar si la suma dona negatiu, 0, 1 o major que 1
+    }
+}
