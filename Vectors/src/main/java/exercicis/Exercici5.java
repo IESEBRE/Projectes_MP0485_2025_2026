43950c24e76876e6c8d7ccf628650a1bf4ac0e29
Santi Sabaté <ssabate@iesebre.com>
2025-11-04 10:05:04 +0100

    Classe del 4/11/2025, explicacions de vectors i exercici 5

---
+++ Vectors/src/main/java/exercicis/Exercici5.java
@@ -0,0 +1,42 @@
+package exercicis;
+
+public class Exercici5 {
+
+    static void main() {
+
+        //Declaració de variables
+        int quantitat;              //quantitat de valors a tractar (triat per l'usuari)
+        int[] vector;               //on guardarem els valors introduits per l'usuari --> no sabem encara la quantitat de caselles necessàries
+        int suma=0;                 //on guardarem la suma dels valors introduits (per poder calcular la mitjana)
+        double mitjana;             //on guardarem la mitjana
+
+        //Demanem a l'usuari que escrigue una quantitat de valors
+        do{
+            quantitat=Integer.parseInt(IO.readln("Quants valors vols introduir (>=0): "));
+            if(quantitat<0) IO.println("Ha de ser major o igual que 0!!");
+            else break;
+        }while(true);
+
+        //només demano valors en cas que quantitat sigue més gran que 0
+        if(quantitat>0) {
+            //Ara que ja sabem la quantitat de valors, podem instanciar el vector
+            vector = new int[quantitat];
+
+            //Ara demanem els "quantitat" valors a l'usuari
+            IO.println(String.format("Introduix %d valors enters:", quantitat));
+            for (int i = 0; i < vector.length; i++) {
+                vector[i] = Integer.parseInt(IO.readln());
+                suma += vector[i];        //suma=suma+vector[i]
+            }
+
+            //Calculem la mitjana
+            mitjana=(double)suma/quantitat;
+
+            //Recorrem de nou el vector i mostrem els majors o iguals que la mitjana
+            IO.println(String.format("Els valors majors o iguals que %f són:", mitjana));
+            for (int i = 0; i < vector.length; i++) {
+                if(vector[i] >= mitjana) IO.println(vector[i]);
+            }
+        }
+    }
+}
--- Vectors/src/main/java/vectors/Vectors1.java
+++ Vectors/src/main/java/vectors/Vectors1.java
@@ -1,4 +1,33 @@
 package vectors;
 
 public class Vectors1 {
+
+
+    static void main() {
+        //Declaració de variables
+        int a,b,c,d,e;
+        b=23;
+        //El mateix fet en un sol vector
+        int[] vector;                       //declarem un vector d'enters
+        vector=new int[5];                  //instanciem el vector en un objecte de 5 caselles enteres
+
+        //Una altre vector declarat instanciat en una sola instrucció
+        char[] text= new char[2];
+
+        //vector d'objectes
+        String[] objectes=new String[1];
+
+        //Recorrem un dels vectors
+        for (int i = 0; i < vector.length; i++) {
+            IO.println(vector[i]);
+        }
+
+        //Miro la longitud del vector
+        a=vector.length;
+        IO.println(String.format("La longitud del vector és %d", a));
+    }
+
+
+
+
 }
